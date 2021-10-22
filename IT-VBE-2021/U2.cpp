350c2d2d81b1e1a79a42a49b0f9873fb0baeb43d
Dovidas <70913423+Dovias@users.noreply.github.com>
2021-10-22 22:36:35 +0300

    2 užduoties atnaujinimas
    Naujas užduoties sprendimo būdas, kuris atitinka visus egzamino užduoties nurodymus, kurie nurodyti lape.

--- IT-VBE-2021/U2.cpp
+++ IT-VBE-2021/U2.cpp
@@ -1,124 +1,86 @@
 #include <fstream>
-#include <cstring>
+#include <string>
 
-#define MAX_STUDENT_AMOUNT 50
-
-#define MAX_STUDENT_NAME 40
-#define MAX_SUBJECT_NAME 40
-#define MAX_STUDENT_GRADES 50
-
-struct StudentData {
-    char firstName[MAX_STUDENT_NAME];
-    char subjectName[MAX_SUBJECT_NAME];
-
-    int studentGrades[MAX_STUDENT_GRADES];
-    int studentsGradesSize;
-
-    bool found = false;
+// Programoje naudokite duomenu tipo strukturos masyva apklaustu mokiniu duomenims saugoti.
+struct Subject {
+    // Naudojame std::string, nes vardu dydziai nezinomi.
+    std::string name;
+    std::string students[50];
+    int sAmount = 0;
 };
 
-struct SubjectData {
-    StudentData* studentData[MAX_STUDENT_AMOUNT];
-    int studentDataSize = 0;
-};
-
-bool loadData(const char* path, StudentData data[], int& dataSize);
-bool saveData(const char* path, SubjectData resultData[], int resultDataSize);
-void calculateData(StudentData data[], int dataSize, SubjectData resultData[], int& resultDataSize);
-void sortData(SubjectData data[], int dataSize);
+bool loadData(const char* path, Subject data[], int& dataSize);
+bool saveData(const char* path, Subject data[], int dataSize);
+void sortData(Subject data[], int dataSize, Subject* sortData[]);
 
 int main() {
-    StudentData data[MAX_STUDENT_AMOUNT];
+    Subject data[50];
     int dataSize;
-    if (!loadData("U2.txt", data, dataSize)) return 1;
-
-    SubjectData resultData[MAX_STUDENT_AMOUNT];
-    int resultDataSize;
-    calculateData(data, dataSize, resultData, resultDataSize);
-
-    sortData(resultData, resultDataSize);
-
-    return saveData("U2rez.txt", resultData, resultDataSize);
-
+    if (!loadData("U2.txt", data, dataSize)) return 0;
+    return !saveData("U2rez.txt", data, dataSize);
 }
 
-bool loadData(const char* path, StudentData data[], int& dataSize) {
-    std::ifstream stream(path);
-    if (!stream.is_open()) return false;
-    stream >> dataSize;
-    for (int i = 0; i < dataSize; i++) {
-        StudentData studentData;
-        stream >> studentData.firstName >> studentData.subjectName >> studentData.studentsGradesSize;
-        for (int j = 0; j < studentData.studentsGradesSize; j++) {
-            stream >> studentData.studentGrades[j];
-        }
-        data[i] = studentData;
-    }
-    // Destruktorius uzdaro failo saltini, nes stream objektas yra lokalus kintamasis, kuris iseina is funkcijos.
-    return true;
-}
-
-bool saveData(const char* path, SubjectData resultData[], int resultDataSize) {
+bool saveData(const char* path, Subject data[], int dataSize) {
     std::ofstream stream(path);
     if (!stream.is_open()) return false;
-    if (resultDataSize == 0) {
+    if (dataSize == 0) {
         stream << "Neatitinka vidurkis";
-        return false;
+        return true;
     }
-    for (int i = 0; i < resultDataSize; i++) {
-        stream << resultData[i].studentData[0]->subjectName << " " << resultData[i].studentDataSize << "\n";
-
-        for (int j = 0; j < resultData[i].studentDataSize; j++) {
-            stream << resultData[i].studentData[j]->firstName << "\n";
-        }
-
+    Subject* sortedData[dataSize];
+    sortData(data, dataSize, sortedData);
+    // Nesvarus isspausdinimas, reikia sugalvoti geresni buda isvengti papildomo \n gale.
+    stream << sortedData[0]->name << ' ' << sortedData[0]->sAmount;
+    for (int j = 0; j < sortedData[0]->sAmount; j++) {
+        stream << '\n' << sortedData[0]->students[j] ;
     }
-    // Destruktorius uzdaro failo saltini, nes stream objektas yra lokalus kintamasis, kuris iseina is funkcijos.
-    return true;
-
-}
-
-void calculateData(StudentData data[], int dataSize, SubjectData resultData[], int& resultDataSize) {
-    resultDataSize = 0;
-    for (int i = 0; i < dataSize; i++) {
-        //Pazymetus kaip jau surastus objektus nebeieskoti per naujo, nes jie jau yra masyve per kitus iterationus.
-        if (data[i].found) {
-            data[i].found = false;
-            continue;
-        }
-        SubjectData subjectData;
-        for (int j = i; j < dataSize; j++) {
-            if (strcmp(data[i].subjectName, data[j].subjectName) != 0) {
-                continue;
-            }
-            data[j].found = true;
-            int average = 0;
-            for (int k = 0; k < data[j].studentsGradesSize; k++) {
-                average += data[j].studentGrades[k];
-            }
-            if (average / data[j].studentsGradesSize < 9) {
-                continue;
-            }
-            subjectData.studentData[subjectData.studentDataSize++] = &data[j];
-        }
-        if (subjectData.studentDataSize != 0) {
-            resultData[resultDataSize++] = subjectData;
-        }
-    }
-}
-
-// Insertion sort algoritmas rusiuojantis duomenis pagal dydi.
-void sortData(SubjectData data[], int dataSize) {
     for (int i = 1; i < dataSize; i++) {
-        for (int j = 0; j < i; j++) {
-            if (data[i].studentDataSize > data[i].studentDataSize) {
-                continue;
-            }
-            SubjectData temp = data[i];
-            for (int k = i+1; --k > j;) {
-                data[k] = data[k-1];
-            }
-            data[j] = temp;
+        stream << '\n' << sortedData[i]->name << ' ' << sortedData[i]->sAmount;
+        for (int j = 0; j < sortedData[i]->sAmount; j++) {
+            stream << '\n' << sortedData[i]->students[j] ;
         }
+    }
+    return true;
+}
+
+bool loadData(const char* path, Subject data[], int& dataSize) {
+    std::ifstream stream(path);
+    if (!stream.is_open()) return false;
+    int fileDataSize;
+    stream >> fileDataSize;
+    dataSize = 0;
+    for (int i = 0; i < fileDataSize; i++) {
+        int gradeAmount;
+        int gradeAvg = 0;
+        stream >> data[dataSize].students[0] >> data[dataSize].name >> gradeAmount;
+        data[dataSize].sAmount = 1;
+        for (int j = 0; j < gradeAmount; j++) {
+            int grade;
+            stream >> grade;
+            gradeAvg += grade;
+        }
+        if (gradeAvg / gradeAmount < 9) continue;
+        for (int j = dataSize-1; j > -1; j--) {
+            if (data[j].name == data[dataSize].name) {
+                data[j].students[data[j].sAmount++] = data[dataSize].students[0];
+                dataSize--;
+                break;
+            }
+        }
+        dataSize++;
+    }
+    return true;
+}
+
+// Sukurkite ir parasykite funkcija, kuri surikiuotu populiariausiu dalyku sarasa. (Insertion sort algoritmas)
+void sortData(Subject data[], int dataSize, Subject* sData[]) {
+    sData[0] = &data[0];
+    for (int i = 0; i < dataSize; i++) {
+        int j = i-1;
+        while (j >= 0 && (data[i].sAmount > sData[j]->sAmount || data[i].sAmount == data[j].sAmount && data[i].name < data[j].name)) {
+            sData[j+1] = sData[j];
+            j--;
+        }
+        sData[j+1] = &data[i];
     }
 }
